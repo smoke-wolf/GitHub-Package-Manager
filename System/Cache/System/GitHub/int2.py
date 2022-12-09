ccf7ba33e4c545e869bb6dabe21c57ddf2e493aa
AshtonLoser <71799746+smoke-wolf@users.noreply.github.com>
2022-12-09 08:13:48 -0500

    Update int2.py

--- System/Cache/System/GitHub/int2.py
+++ System/Cache/System/GitHub/int2.py
@@ -1,2 +1,1 @@
-main = "+python3 /Users/maliq/PycharmProjects/SwAT_Model/System/Cache/System/GitHub/Downloads/weather/main.py-"
-Captin = "+python3 /Users/maliq/PycharmProjects/SwAT_Model/System/Cache/System/GitHub/Downloads/Captian/Captin.py-"
+
