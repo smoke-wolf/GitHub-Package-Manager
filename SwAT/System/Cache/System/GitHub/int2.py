51312288a494ca9cbeadcf0fecacd3d6f3b56248
AshtonLoser <71799746+smoke-wolf@users.noreply.github.com>
2022-12-09 08:12:00 -0500

    Delete SwAT/System directory

--- SwAT/System/Cache/System/GitHub/int2.py
+++
@@ -1,2 +0,0 @@
-main = "+python3 /Users/maliq/PycharmProjects/SwAT_Model/System/Cache/System/GitHub/Downloads/weather/main.py-"
-Captin = "+python3 /Users/maliq/PycharmProjects/SwAT_Model/System/Cache/System/GitHub/Downloads/Captian/Captin.py-"
