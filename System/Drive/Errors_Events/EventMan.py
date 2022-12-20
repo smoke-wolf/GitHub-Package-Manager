9350a6fb46e034ee522e19a4b6761f0a9e4103e7
AshtonLoser <71799746+smoke-wolf@users.noreply.github.com>
2022-12-20 15:21:43 -0500

    Add files via upload

---
+++ System/Drive/Errors_Events/EventMan.py
@@ -0,0 +1,8 @@
+import base64, codecs
+magic = 'aW1wb3J0IFVzZXIuVXNlclByb2ZpbGUKaW1wb3J0IG9zCmN3ZCA9IG9zLmdldGN3ZCgpCmRlZiBJc3N1ZUlEKExpbmUpOgogICAgTGluZSA9IGludChMaW5lKQogICAgSXNzdWVJRCA9IGhleChMaW5lKQogICAgZ2xvYmFsIElzSUQKICAgIElzSUQgPSBJc3N1ZUlECgpkZWYgTmV3RXZlbnQo'
+love = 'MKMyoaDfVSOioPx6PvNtVPOSqzIhqSWypT9lqPN9VT9jMJ4bMvq7L3qxsF9GrKA0MJ0iD2SwnTHiH3ymqTIgY0Ilpz9lGT9aY0I2MJ50plpfVPquWlxXVPNtVTygpT9lqPOxLKEyqTygMDbtVPNtnJLtHT9fVQ09VQN6PvNtVPNtVPNtpUWcoaDbMKMyoaDcPvNtVPOyoUAyBtbtVPNtVPNtVUOu'
+god = 'c3MKCiAgICBFdmVudFRpbWUgPSBkYXRldGltZS5kYXRldGltZS5ub3coKQogICAgRXJObyA9IGludChQb2wpCiAgICBDUklTID0gRmFsc2UKICAgIGlmIEVyTm8gPT0gMToKICAgICAgICBDUklTID0gJ1VzZXIgICcKICAgIGVsaWYgRXJObyA9PSAwOgogICAgICAgIENSSVMgPSAnU3lzdGVt'
+destiny = 'WjbtVPNtEKMyoaEHnJ1yVQ0tp3ElXRI2MJ50ITygMFyoBv03KDbtVPNtHzIjo3W0VQ0tMvpaW0I2MJ50VTS0VUgSqzIhqSEcoJI9BagQHxyGsFOoVI17MKMyoaE9JlSqWlpaPvNtVPOSqzIhqSWypT9lqP53pzy0MFuzW1khr1WypT9lqU0aXDbtVPNtEKMyoaEFMKOipaDhL2kip2HbXDbXPt=='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
