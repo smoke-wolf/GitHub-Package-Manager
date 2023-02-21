0ac556b5e7ac2265451086da0ffd35809e0e7dc7
JDX50S <71799746+smoke-wolf@users.noreply.github.com>
2023-02-20 19:07:46 -0500

    Update ModuleVerifier.py

--- System/Drive/ModuleVerifier.py
+++ System/Drive/ModuleVerifier.py
@@ -2,38 +2,42 @@
 import os
 import sys
 
-import System.Requirements.Information
-import User.UserProfile
+try:
+    import System.Requirements.Information
+    import User.UserProfile
+except:
+    pass
 
 try:
     if User.UserProfile.Force_Import_Request is False:
         import System.Drive.Errors_Events.EventMan as EV
-        EV.NewEvent(event=f'Import Test Aborted Due To User Settings', Pol=0)
-    else:
-        raise ValueError
 
-except ValueError:
+        EV.NewEvent(event=f"Import Test Aborted Due To User Settings", Pol=0)
+    else:
+        pass
+
+except:
     Modules = System.Requirements.Information.Modules
     for Module in Modules:
         try:
             __import__(Module)
-            print("Successfully imported ", Module, '.')
+            print("Successfully imported ", Module, ".")
             import System.Drive.Errors_Events.EventMan as EV
 
-            EV.NewEvent(event=f'Import_Success = {Module}', Pol=0)
+            EV.NewEvent(event=f"Import_Success = {Module}", Pol=0)
         except ImportError:
-            print("Error importing ", Module, '.')
+            print("Error importing ", Module, ".")
             import System.Drive.Errors_Events.EventMan as EV
 
-            EV.NewEvent(event=f'Import_Failed = {Module}', Pol=0)
+            EV.NewEvent(event=f"Import_Failed = {Module}", Pol=0)
             try:
-                os.system(f'pip install {Module}')
-                os.system(f'pip3 install {Module}')
+                os.system(f"pip install {Module}")
+                os.system(f"pip3 install {Module}")
             except:
-                print('Module failed to install')
+                print("Module failed to install")
                 sys.exit(0)
 
-
     import time
+
     time.sleep(1)
-    print('\n'*100)
+    print("\n" * 100)
