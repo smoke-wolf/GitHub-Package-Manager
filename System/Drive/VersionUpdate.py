48a71f8187fd356a71091997184d918b2226798a
JDX50S <71799746+smoke-wolf@users.noreply.github.com>
2023-08-22 18:47:49 -0700

    Add files via upload

---
+++ System/Drive/VersionUpdate.py
@@ -0,0 +1,64 @@
+import shutil
+import os
+import sys
+
+import User.UserProfile as UserProfile
+
+import requests
+try:
+    shutil.rmtree(f'{UserProfile.SourceDirectory}System/.Cache/System/Update/GitHub-Package-Manager')
+except:
+    pass
+
+def replace_and_remove(directory_a, directory_b):
+    # Copy the content of directory B to directory A
+    shutil.rmtree(directory_a)  # Remove existing content of directory A
+    shutil.copytree(directory_b, directory_a)
+
+    # Remove directory B
+    shutil.rmtree(directory_b)
+
+
+url = "https://raw.githubusercontent.com/smoke-wolf/GitHub-Package-Manager/INFO/System/Cache/System/COMPATABLE"
+
+response = requests.get(url)
+if response.status_code == 200:
+    comp = response.text
+
+    with open(
+            f'{UserProfile.SourceDirectory}System/.Cache/System/Version.py',
+            'r',
+    ) as ver:
+        global CurrentVersion
+        CurrentVersion = ver.read()
+
+        print(CurrentVersion[11:-2])
+
+    if CurrentVersion[11:-2] in comp:
+        pass
+    else:
+        print('No Updates Available')
+        sys.exit(0)
+else:
+    print(f"Failed to fetch content from {url}. Status code: {response.status_code}")
+
+#   Try Update
+
+url = "https://raw.githubusercontent.com/smoke-wolf/GitHub-Package-Manager/INFO/System/Cache/System/Version.py"
+
+response = requests.get(url)
+if response.status_code == 200:
+    content = response.text
+    # Now, the content of the URL is stored in the 'content' variable
+    print(f'Welcome to the newest version: {content[10:]}')  # You can print it or process it further
+    with open(f'{UserProfile.SourceDirectory}System/.Cache/System/Version.py','w') as w:
+        input('Enter to Continue with update.')
+        w.write(content)
+
+else:
+    print(f"Failed to fetch content from {url}. Status code: {response.status_code}")
+
+os.chdir(f'{UserProfile.SourceDirectory}System/.Cache/System/Update')
+os.system('git clone -b UPDATE https://github.com/smoke-wolf/GitHub-Package-Manager.git')
+replace_and_remove(f'{UserProfile.SourceDirectory}System/Drive',
+                   f'{UserProfile.SourceDirectory}System/.Cache/System/Update/GitHub-Package-Manager/System/Drive')
