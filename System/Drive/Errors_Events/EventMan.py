408a6d1583ec48f4d77791855e00d3ac98c2ff4d
JDX50S <71799746+smoke-wolf@users.noreply.github.com>
2023-08-24 17:56:50 -0700

    Update EventMan.py

--- System/Drive/Errors_Events/EventMan.py
+++ System/Drive/Errors_Events/EventMan.py
@@ -1,6 +1,7 @@
 import datetime
 import socket
 import time
+import requests
 
 pileup_push = 0
 
@@ -143,24 +144,62 @@ def guiEvent(typeerror, event, line, address, terminate=False, record=True, seve
         checktype()
     else:
         pass
+
 def PushAnalytics(a1,a2,a3):
+
     if User.UserProfile.PushLogs:
+        AnalyticsRecord(9)
         import requests
         global analytics_push_id
         analytics_push_id = 0
         ip = socket.gethostbyname(socket.gethostname())
-     
+
         url = f"https://gpm-web.vercel.app/push={analytics_push_id}/usr={User.UserProfile.Username}/ip{ip}/requesttype={a2}/aditional={a3}"
         requests.get(url)
-        analytics_push_id += 1
-    
-        logform = f'''
-    ==
-    -{datetime.datetime.now()}
-    --{a1}
-    ---{analytics_push_id}
-    ----{a2}
-    ==
-    '''
-        with open(f'{cwd}System/Drive/Errors_Events/Push/Log', 'a') as r:
-            r.write(logform)
+
+def AnalyticsRecord(a1):
+    push_types = ['install-github0', 'install-local1', 'install-github-complex2', 'activate-local3', 'activate-github4',
+                  'activate-github-complex5', 'activator-update6', 'activator-reinstall7', 'settings8', 'send_logs9',
+                  'auto_update10','advertisement11']
+
+    bref = f'[{a1}]'
+    if os.path.exists(f'{User.UserProfile.SourceDirectory}System/.Cache/User/analytics'):
+        with open(f'{User.UserProfile.SourceDirectory}System/.Cache/User/analytics', 'a') as file:
+            # Write content to the file
+            file.write(f"{bref}")
+        from datetime import datetime
+
+        def is_same_date_as_today(file_path):
+            # Get today's date in '%Y-%m-%d' format
+            today_date = datetime.today().strftime('%Y-%m-%d')
+
+            # Read the first line of the file
+            with open(file_path, 'r') as file:
+                first_line = file.readline().strip()
+
+            # Compare the first line of the file with today's date
+            return first_line == today_date
+
+        with open(f'{User.UserProfile.SourceDirectory}System/.Cache/User/analytics', 'r') as file:
+            data = file.read()
+            if is_same_date_as_today(f'{User.UserProfile.SourceDirectory}System/.Cache/User/analytics'):
+                url = f'''analytics={data}'''
+                requests.get(url)
+
+
+    else:
+        with open(f'{User.UserProfile.SourceDirectory}System/.Cache/User/analytics', 'w') as file:
+            # Write content to the file
+            from datetime import datetime, timedelta
+
+            # Get the current date
+            today = datetime.today()
+
+            # Calculate the date one month from today
+            one_month_later = today + timedelta(days=10)  # Approximation for one month
+
+            # Format the date as '%Y-%m-%d'
+            formatted_date = one_month_later.strftime('%Y-%m-%d')
+
+            file.write(f"{formatted_date}\n")
+            file.write(f"{bref}")
