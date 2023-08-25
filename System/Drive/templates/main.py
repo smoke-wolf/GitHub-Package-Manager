2e04a67dae612e52354347cd6c372a351a5671e2
JDX50S <71799746+smoke-wolf@users.noreply.github.com>
2023-08-24 17:59:53 -0700

    Update main.py

--- System/Drive/templates/main.py
+++ System/Drive/templates/main.py
@@ -1,7 +1,9 @@
 import os
 import User.UserProfile as up
+import System.Drive.Errors_Events.EventMan as AR
 
 def main():
+    AR.AnalyticsRecord(11)
     os.system(
         f'osascript -e \'tell application "Terminal" to do script "cd {up.SourceDirectory}System/Drive/templates &&python3 {up.SourceDirectory}System/Drive/templates/launchwebpage.py"\''
     )
