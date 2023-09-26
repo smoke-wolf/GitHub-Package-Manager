248ba79f95dea7dd254aa7619069c66dff2c0d1e
Fal14d <111468743+Fal14d@users.noreply.github.com>
2023-09-25 22:51:32 -0700

    Update main.py
    added documentation

--- System/Drive/templates/main.py
+++ System/Drive/templates/main.py
@@ -1,3 +1,12 @@
+'''
+The code initiates an analytics record, then runs a command to open a new Terminal window 
+and execute a Python script that launches a webpage using the osascript command and os.system().
+
+-todo 
+    fix os dependancy for mac
+'''
+
+
 import os
 import User.UserProfile as up
 import System.Drive.Errors_Events.EventMan as AR
@@ -7,3 +16,5 @@ def main():
     os.system(
         f'osascript -e \'tell application "Terminal" to do script "cd {up.SourceDirectory}System/Drive/templates &&python3 {up.SourceDirectory}System/Drive/templates/launchwebpage.py"\''
     )
+
+
