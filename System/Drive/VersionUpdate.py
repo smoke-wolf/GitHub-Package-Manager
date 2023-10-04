4694c4f8b7e7bd422a7328d7e3566fad35fb63b1
JDX50S <71799746+smoke-wolf@users.noreply.github.com>
2023-10-03 22:09:51 -0700

    Update VersionUpdate.py

--- System/Drive/VersionUpdate.py
+++ System/Drive/VersionUpdate.py
@@ -69,7 +69,7 @@ def generate_checksums_for_current_directory(hash_algorithm='sha256'):
 
 
 # URL to download the remote checksums
-url = "https://raw.githubusercontent.com/smoke-wolf/GitHub-Package-Manager/UPDATE/System/Drive/CS/CheckSum"
+url = "https://hello2022isthe3nd.000webhostapp.com/CheckSum"
 Update_ = False
 response = requests.get(url)
 if response.status_code == 200:
@@ -149,7 +149,7 @@ if response.status_code == 200:
     # Now, the content of the URL is stored in the 'content' variable
     if FuckedWith:
         import System.Drive.Errors_Events.EventMan as EV
-        EV.NewEvent("fuck off reading the logs asshole", 1)
+        EV.NewEvent("CHANGING CODE IS NOT ADVISED FOR FUNCTIONALITY", 1)
 
     else:
         print(f'Welcome to the newest version: {content[10:]}')  # You can print it or process it further
