7f0396506bbf550b32f3066b0b3306f6f12004a8
JDX50S <71799746+smoke-wolf@users.noreply.github.com>
2023-11-05 14:24:21 -0800

    Update formbuilder.py

--- System/Drive/UI_Functions/UI_Builder/formbuilder.py
+++ System/Drive/UI_Functions/UI_Builder/formbuilder.py
@@ -1,5 +1,4 @@
 from PIL import Image, ImageTk
-from tkinter import ttk
 import tkinter as tk
 from . import iconbuttons as ib
 from . import scrollablelabel as sl
@@ -8,35 +7,34 @@ class CustomFrame:
 
     def __init__(self, bgImage=None):
         self.frame = tk.Frame()
-        
-        if bgImage != None:
+
+        if bgImage is not None:
             self.canvas = tk.Canvas(self.frame, width=1000, height=650, bg="white")
             self.canvas.pack(fill="both")
 
             self.file = Image.open(bgImage)
-            self.img = ImageTk.PhotoImage(self.file.resize((464, 164), Image.ANTIALIAS))
+            self.img = ImageTk.PhotoImage(self.file.resize((464, 164)))
             self.bg = self.canvas.create_image(300, 0, anchor=tk.NW, image=self.img)
             self.bgCheck = True
+        else:
+            self.bgCheck = False
 
-        self.bgCheck = False
         self.widgets = []
-    
-    def setupLabel(self, x, y, width, height, filePath=None, text=None):
+
+    def setupLabel(self, x, y, width, height, text=None):
         if self.bgCheck:
             label = sl.ScrollableLabel(self.canvas, x, y, text)
-            label.configure(width, height)
+            label.configure(width=width, height=height)
             self.labelWindow = self.canvas.create_window(x, y, anchor=tk.NW, window=label.frame1)
-        
         else:
             label = sl.ScrollableLabel(self.frame, x, y, text)
-            label.configure(width, height)
+            label.configure(width=width, height=height)
             self.widgets.append(label)
 
     def setupIconButton(self, x, y, iconPath, iconScale=1, command=lambda: print("Hi Mom!!")):
         if self.bgCheck:
             button = ib.IconButton(self.canvas, x, y, command, iconPath, iconScale)
-            self.buttonWindow = self.canvas.create_window(x, y, anchor=tk.NW, window=button)
-        
+            self.buttonWindow = self.canvas.create_window(x, y, anchor=tk.NW)
         else:
             button = ib.IconButton(self.frame, x, y, command, iconPath, iconScale)
             self.widgets.append(button)
