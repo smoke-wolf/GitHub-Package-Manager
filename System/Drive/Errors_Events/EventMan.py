317701c839efae4b3ea04a164ed2b9029a353bbf
JDX50S <71799746+smoke-wolf@users.noreply.github.com>
2023-11-05 00:08:44 -0700

    Add files via upload

---
+++ System/Drive/Errors_Events/EventMan.py
@@ -0,0 +1,351 @@
+import datetime
+import inspect
+import time
+import requests
+import socket
+import User.UserProfile
+import os
+
+tryagain = True
+def display_notification(title, message):
+    applescript = f'display notification "{message}" with title "{title}"'
+    os.system(f"osascript -e '{applescript}'")
+
+
+try:
+    response = requests.get("https://www.google.com", timeout=1)
+    if response.status_code == 200:
+        pass
+except:
+    display_notification("Hold Up!", "To continue using ghpm. Please connect to the internet")
+
+
+    def check_internet_connection():
+        try:
+            response = requests.get("https://www.google.com", timeout=5)
+            return response.status_code == 200
+        except requests.ConnectionError:
+            return False
+
+
+    while True:
+        if check_internet_connection():
+            # Internet connection is available, display thank you message
+            display_notification("Thank you!", " Please continue enjoying ghpm!!")
+
+            break  # Exit the loop
+        else:
+            # Internet connection is not available, wait for a while and check again
+            time.sleep(0.5)  # Wait for 5 seconds before checking again
+
+pileup_push = 0
+avg_t = []
+numr = 0
+
+try:
+    import User.UserProfile
+
+    cwd = User.UserProfile.SourceDirectory
+except:
+    cwd = os.getcwd()
+
+
+def IssueID(Line):
+    Line = int(Line)
+    IssueID = hex(Line)
+    global IsID
+    IsID = IssueID
+
+
+def NewEvent(event, Pol):
+    import sqlite3 as sl
+
+    try:
+        con = sl.connect(
+            f'{os.getcwd()}/System/.Cache/System/ErrorLog/Event.db'
+        )
+        with con:
+            con.execute(
+                """
+                    CREATE TABLE Event (
+                        Event TEXT,
+                        Time TEXT,
+                        Type TEXT
+                    );
+                """
+            )
+    except:
+        pass
+    #
+    try:
+        EventReport = open(f'{cwd}/System/.Cache/System/ErrorLog/Events', 'a')
+    except:
+        EventReport = open(f'{cwd}System/.Cache/System/ErrorLog/Events', 'a')
+
+    import datetime
+
+    if Pol == 10:
+        print(event)
+    elif Pol == 20:
+        print(event)
+    else:
+        pass
+    try:
+        if User.UserProfile.DisplayEvents is True:
+            if Pol == 1:
+                print(event)
+            else:
+                pass
+        else:
+            pass
+    except:
+        pass
+
+    EventTime = datetime.datetime.now()
+    ErNo = int(Pol)
+    CRIS = False
+    if ErNo == 1:
+        CRIS = 'User'
+    elif ErNo == 0:
+        CRIS = 'System'
+    else:
+        CRIS = None
+
+    def Update(data):
+        sql = 'INSERT INTO Event (Event, Time, Type) values(?, ?, ?)'
+
+        with con:
+            con.executemany(sql, data)
+
+    Et = str(EventTime)[:-7]
+    
+    try:
+        Update(data=[(f'{event}', f'{Et}', f'{CRIS}')])
+    except:
+        pass
+    if CRIS is None:
+        try:
+            exec(event)
+        except:
+            pass
+    else:
+        EventTime = str(EventTime)[:-7]
+        Report = f"""Event at {EventTime}:{CRIS} --> {event}"""
+        EventReport.write(f'\n{Report}')
+        EventReport.close()
+    push_analytics_subprocess(0, User.UserProfile.Username, event)
+
+
+def guiEvent(typeerror, event, line, address, terminate=False, record=True, severity=0):
+    date = datetime.datetime.now()
+
+    def checktype():
+        global typeer
+        if typeerror == 1:
+            typeer = 'User'
+        elif typeerror == 2:
+            typeer = 'SYSTEM ERROR'
+        elif typeerror == 0:
+            typeer = 'System'
+        elif typeerror == 3:
+            typeer = 'Notice'
+        elif typeerror == 4:
+            typeer = 'CheckPoint'
+
+        Record(typeer)
+
+    def Record(typeer):
+        try:
+            display_format = f'{typeer} --> {event} @ line:{line} severity:{severity}'
+            if typeer == 'CheckPoint':
+                display_format = f'{typeer} line:{line} function:{address}'
+                
+            if User.UserProfile.DisplayEvents is True:
+                print(display_format)
+            else:
+                pass
+                
+            if User.UserProfile.PushLogs:
+                st = time.time()
+                push_analytics_subprocess(0, User.UserProfile.Username, display_format)
+                avg_t.append(time.time() - st)
+                total = sum(avg_t)
+                average = total / len(avg_t)
+                try:
+                    if User.UserProfile.AdvancedL:
+                        try:
+                            average = str(average)
+                        except:
+                            print(f'average time -> {average[:-3]}')
+
+                except:
+                    return EOFError
+
+        except:
+            print('Network Connection Error')
+        logfile = f'{User.UserProfile.SourceDirectory}System/.Cache/System/ErrorLog/GUIevents'
+        with open(logfile, 'a') as log:
+            log.write(f'\n{display_format}')
+        try:
+            if User.UserProfile.LV:
+                logfile = f'{User.UserProfile.SourceDirectory}UIevents'
+                with open(logfile, 'a') as log:
+                    log.write(f'\n{display_format}')
+            else:
+                pass
+        except:
+            pass
+
+    if record:
+        checktype()
+    else:
+        pass
+
+
+def get_current_function():
+    stack = inspect.stack()
+    frame = stack[1]
+    code = frame[0]
+    return code.f_code.co_name
+
+
+def push_analytics_subprocess(a1, a2, a3):
+    tryagain = True
+    try:
+        if tryagain:
+            try:
+                if User.UserProfile.PushLogs:
+                    AnalyticsRecord(9)
+                    global analytics_push_id
+                    analytics_push_id = 0
+                    ip = socket.gethostbyname(socket.gethostname())
+
+                    import requests
+
+                    # Define the base URL
+                    base_url = f"https://hello2022isthe3nd.000webhostapp.com/eventlogger.php?data1={analytics_push_id}&data2={User.UserProfile.Username}&data3={ip}&data4={a2}&data5={a3}"
+
+
+
+                    # Send a GET request with the parameters
+                    requests.get(base_url)
+            except:
+                try:
+                    lm = User.UserProfile.OFFLINE
+                except:
+                    lm = False
+
+                if lm is False:
+                    def display_notification(title, message):
+                        applescript = f'display notification "{message}" with title "{title}"'
+                        os.system(f"osascript -e '{applescript}'")
+
+                    try:
+                        response = requests.get("https://www.google.com", timeout=1)
+                        if response.status_code == 200:
+                            pass
+                    except:
+                        display_notification("Hold Up!", "To continue using ghpm. Please connect to the internet")
+
+                        def check_internet_connection():
+                            try:
+                                response = requests.get("https://www.google.com", timeout=5)
+                                return response.status_code == 200
+                            except requests.ConnectionError:
+                                return False
+
+                        while True:
+                            if check_internet_connection():
+                                # Internet connection is available, display thank you message
+                                display_notification("Thank you!", " Please continue enjoying ghpm!!")
+
+                                break  # Exit the loop
+                            else:
+                                # Internet connection is not available, wait for a while and check again
+                                time.sleep(0.5)  # Wait for 5 seconds before checking again
+
+    except:
+        guiEvent(2, 'Network Issue With Log Push', inspect.currentframe().f_lineno, get_current_function(), False, True, 1)
+        return None
+
+
+def PushAnalytics(a1, a2, a3):
+    st = time.time()
+    push_analytics_subprocess(a1, a2, a3)
+    avg_t.append(time.time() - st)
+    total = sum(avg_t)
+    average = total / len(avg_t)
+    average = str(average)
+    try:
+        if User.UserProfile.AdvancedL:
+
+            print(f'average time -> {average[:3]}')
+
+    except:
+        pass
+
+def AnalyticsRecord(a1):
+
+    push_types = ['install-github0', 'install-local1', 'install-github-complex2', 'activate-local3', 'activate-github4',
+                  'activate-github-complex5', 'activator-update6', 'activator-reinstall7', 'settings8', 'send_logs9',
+                  'auto_update10', 'advertisement11']
+
+    bref = f'[{a1}]'
+
+
+    try:
+        int(a1)
+    except:
+        if User.UserProfile.DisplayEvents:
+            NewEvent(f'NOTICE: {a1}', 1)
+
+
+    if os.path.exists(f'{User.UserProfile.SourceDirectory}System/.Cache/User/analytics'):
+        with open(f'{User.UserProfile.SourceDirectory}System/.Cache/User/analytics', 'a') as file:
+            # Write content to the file
+            file.write(f"{bref}")
+        from datetime import datetime
+
+        def is_same_date_as_today(file_path):
+            # Get today's date in '%Y-%m-%d' format
+            today_date = datetime.today().strftime('%Y-%m-%d')
+
+            # Read the first line of the file
+            with open(file_path, 'r') as file:
+                first_line = file.readline().strip()
+
+            # Compare the first line of the file with today's date
+            return first_line == today_date
+
+        with open(f'{User.UserProfile.SourceDirectory}System/.Cache/User/analytics', 'r') as file:
+            data = file.read()
+            if is_same_date_as_today(f'{User.UserProfile.SourceDirectory}System/.Cache/User/analytics'):
+                import requests
+
+                # Define the base URL
+                base_url = f"https://hello2022isthe3nd.000webhostapp.com/eventlogger.php?data1={data}&data2={User.UserProfile.Username}&data3=none&data4=mone&data5=None"
+
+
+                # Send a GET request
+                requests.get(base_url)
+
+
+
+
+
+    else:
+        with open(f'{User.UserProfile.SourceDirectory}System/.Cache/User/analytics', 'w') as file:
+            # Write content to the file
+            from datetime import datetime, timedelta
+
+            # Get the current date
+            today = datetime.today()
+
+            # Calculate the date one month from today
+            one_month_later = today + timedelta(days=10)  # Approximation for one month
+
+            # Format the date as '%Y-%m-%d'
+            formatted_date = one_month_later.strftime('%Y-%m-%d')
+
+            file.write(f"{formatted_date}\n")
+            file.write(f"{bref}")
---
+++ System/Drive/UI_Functions/Uninstall.py
@@ -0,0 +1,179 @@
+'''
+this script is part of a larger system and is responsible for presenting
+a list of items for uninstallation to the user, confirming the uninstallation,
+and handling the removal of items. It also sends analytics events related to the uninstallation process.
+'''
+
+
+
+
+import datetime
+import inspect
+import tkinter as tk
+from tkinter import ttk
+import User.UserProfile
+import System.Drive.Errors_Events.EventMan as EV
+import os
+import uuid
+
+# Set the current working directory from UserProfile
+current_working_directory = User.UserProfile.SourceDirectory
+
+# Generate a unique UUID and send an analytics event
+unique_id = uuid.uuid1().hex
+EV.PushAnalytics(a1=unique_id, a2='Uninstall', a3='None')
+
+# Function to retrieve the current function's name
+def get_current_function_name():
+    stack = inspect.stack()
+    frame = stack[1]
+    code = frame[0]
+    return code.f_code.co_name
+
+# Function to confirm the uninstallation
+def confirm_uninstall(selected_item):
+    # Send a GUI event to confirm uninstallation
+    EV.guiEvent(4, '', inspect.currentframe().f_lineno, get_current_function_name(), False, True, 1)
+    import tkinter.messagebox
+
+    result = tk.messagebox.askyesno('Confirm Uninstall', f'Are you sure you want to uninstall {selected_item}?')
+
+    # Close the list window
+    list_window.destroy()
+
+    if result:
+        value_index = items.index(selected_item)
+
+        if value_index >= count:
+            dr = int(value_index) - count
+            command = lines2[dr - 1]
+            print(command)
+
+            try:
+                import shutil
+                directory_to_remove = command.split('@', 1)[0]
+                shutil.rmtree(directory_to_remove)
+
+                print(f'Project Removed: {directory_to_remove}')
+
+            except:
+                print(f'Project Failed To Remove: {directory_to_remove}')
+                EV.guiEvent(0, f'{get_current_function_name()} Error: {directory_to_remove} not removed',
+                            inspect.currentframe().f_lineno, os.path.abspath(__file__), False, True, 3)
+
+        else:
+            with open(f'{current_working_directory}System/.Cache/System/GitHub/int.txt', 'r') as file:
+                lines5 = file.readlines()
+
+            with open(f'{current_working_directory}System/.Cache/System/GitHub/int.txt', 'w') as file:
+                for line_in_file in lines5:
+                    if line_in_file.find(selected_item) != -1:
+                        rmdir = line_in_file.split('@')[0]
+                        try:
+                            import shutil
+                            shutil.rmtree(rmdir)
+                            tk.messagebox.showinfo('Success', f'{rmdir} has been uninstalled.')
+                        except:
+                            EV.guiEvent(0, f'{get_current_function_name()} Error: Failed to remove {rmdir}',
+                                        inspect.currentframe().f_lineno, os.path.abspath(__file__), False, True, 3)
+                    else:
+                        file.write(line_in_file)
+
+        return rmdir
+    else:
+        EV.guiEvent(0, 'Deletion Cancelled', inspect.currentframe().f_lineno, get_current_function_name(), False, True, 1)
+
+# Function to display the list of items to uninstall
+def show_list_window():
+    # Send a GUI event to show the list window
+    EV.guiEvent(4, '', inspect.currentframe().f_lineno, get_current_function_name(), False, True, 1)
+    global list_window
+    list_window = tk.Toplevel()
+
+    list_window.title('Uninstall List')
+    screen_width = list_window.winfo_screenwidth()
+    screen_height = list_window.winfo_screenheight()
+    window_width = 440
+    window_height = 460
+    alpha_value = 0.93  # Adjust the alpha value as needed
+
+    list_window.attributes("-alpha", alpha_value)
+
+    # Create a canvas to act as the window's background with a colored rectangle
+    canvas = tk.Canvas(list_window, width=526, height=505)
+
+    # Set the canvas background color
+    bg_color = '#EE85B5'
+    canvas.create_rectangle(0, 0, 526, 505, fill=bg_color, outline="")
+
+    x_coordinate = (screen_width // 2) - (window_width // 2)
+    y_coordinate = (screen_height // 2) - (window_height // 2)
+    list_window.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
+
+    list_box = tk.Listbox(list_window, font=('Helvetica', 12), bd=2, relief=tk.SOLID, selectmode=tk.SINGLE)
+    list_box.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)
+
+    global items
+    items = []
+    try:
+        with open(f'{current_working_directory}System/.Cache/System/GitHub/int.txt', 'r') as original_file, open(f'{current_working_directory}System/.Cache/System/GitHub/int2.txt', 'w') as copy_file:
+            for line in original_file:
+                if line.strip():
+                    copy_file.write(line)
+    except:
+        EV.guiEvent(0, f'{get_current_function_name()} Error: GitHub data is locked or not found',
+                    inspect.currentframe().f_lineno, os.path.abspath(__file__), False, True, 4)
+
+    try:
+        e = open(f'{current_working_directory}System/.Cache/System/GitHub/Complex2', 'r')
+    except:
+        EV.guiEvent(0, f'{get_current_function_name()} Error: GitHub data cannot be read',
+                    inspect.currentframe().f_lineno, os.path.abspath(__file__), False, True, 3)
+
+    f = None  # Initialize f variable
+
+    try:
+        f = open(f'{current_working_directory}System/.Cache/System/GitHub/int2.txt', 'r')
+    except:
+        EV.guiEvent(0, f'{get_current_function_name()} Error: GitHub data cannot be read',
+                    inspect.currentframe().f_lineno, os.path.abspath(__file__), False, True, 3)
+
+    lines = []
+    if f is not None:
+        lines = f.readlines()
+
+    global count
+    count = 0
+    for line in lines:
+        count += 1
+
+    count1 = 0
+    for line in lines:
+        value4 = line.strip()
+        Val = value4.split('&', 1)
+        if len(Val) > 0:
+            value4 = Val[1]
+        items.append(value4)
+        count1 += 1
+
+    try:
+        global lines2
+        lines2 = e.readlines()
+        for line2 in lines2:
+            count1 += 1
+            items.append(line2)
+    except:
+        EV.guiEvent(0, f'{get_current_function_name()} Error: Readlines failed for Complex2',
+                    inspect.currentframe().f_lineno, os.path.abspath(__file__), False, True, 3)
+
+    for item in items:
+        list_box.insert(tk.END, item)
+
+    confirm_button = ttk.Button(list_window, text='Confirm Uninstall',
+                                command=lambda: confirm_uninstall(list_box.get(list_box.curselection())),
+                                style='Custom.TButton')
+    confirm_button.pack(pady=10)
+
+    exit_button = ttk.Button(list_window, text='Exit Uninstaller', command=lambda: list_window.destroy(),
+                             style='Custom.TButton')
+    exit_button.pack(pady=10)
