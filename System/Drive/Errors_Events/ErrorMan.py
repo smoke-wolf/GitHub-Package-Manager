9ea7500584a0927a84da38b25a1d417d394b2ee6
JDX50S <71799746+smoke-wolf@users.noreply.github.com>
2023-11-25 04:53:26 -0800

    Delete System/Drive directory

--- System/Drive/Errors_Events/ErrorMan.py
+++
@@ -1,6 +0,0 @@
-def IssueID(Line):
-    pass
-
-
-def NewIssue(Line, ErNo, SCR, KeFu, UserInp):
-    pass
--- System/Drive/webkit/l003.py
+++
@@ -1,6 +0,0 @@
-
-import os
-os.system("""osascript -e 'tell application "Terminal" to set visible of every window to false'""")
-import System.Drive.UI_Functions.Uninstall as ad
-
-ad.show_list_window()
