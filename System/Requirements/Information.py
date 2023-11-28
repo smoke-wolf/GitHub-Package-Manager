7b7fd80b5bb984b707ec4ec3ba8f0d1db84b3337
JDX50S <71799746+smoke-wolf@users.noreply.github.com>
2023-11-27 17:20:37 -0800

    Reset Version- fun while it lasted!

---
+++ System/Requirements/Information.py
@@ -0,0 +1,62 @@
+SwAT = """
+GHPM is a powerful package manager for GitHub that helps users to organize, compile, and recall packages with ease.
+
+It offers a user-friendly interface, and packages are downloaded and compiled with minimal struggle. 
+GHPM automatically handles requirements and other prerequisites, making it easier for users to install applications and projects.
+
+Compared to other GitHub package managers, GHPM offers unparalleled features such as cross-instance reference of programs, and it's the most lightweight manager available.
+
+Whether you're a beginner or an experienced programmer, GHPM offers the tools you need to take your programming experience to the next level.
+
+GHPM Version = v1.3.5
+"""
+
+Modules = [
+    'os',
+    'platform',
+    'sys',
+    'time',
+    'socket',
+    'random',
+    'json',
+    'hashlib',
+    'base64',
+    'cryptography',
+    'tkinter',
+    'requests',
+]
+
+Functions_Settings = """Settings Info:
+
+The Manual Cache Management feature enables users to interact with system function as well as their own data. 
+Resulting changes to the MCM files will depend on the privilege of the user, as well as the source of the 
+alteration. (System changes will be implemented in (System.Drive), whereas user changes will be implemented 
+in (Cache.USER))
+
+Notation* 
+
+Changes to System Settings will be reflected on package functionality and will result in changes to the user 
+experience. However, changes made to User Settings may not directly affect the experience of the user.
+(User Settings are related to Data Collection and Implementation) """
+
+CLI_COC = """
+To Launch The CLI, In your terminal all you need to do is run gh (arg)
+
+examples:
+    [gh -I https://github.com/SomeUser/Something.git] -> this installs the repo following -I
+    [gh -IL  /Users/Someone/Something] -> this will import all of the files within the specified directory
+
+======================================
+    Help
+    ====
+    -I -> Install (arg<repo>)
+    -IL -> Install Local (arg<dir>)
+
+    -LA -> List All Installs
+    -LL -> Launch Local Directory
+    -LG -> Launch Git Project
+    -LC -> Launch Advanced Projects
+
+    -UG -> Uninstall GitHub Projects
+    -UL -> Uninstall Local directories
+"""
