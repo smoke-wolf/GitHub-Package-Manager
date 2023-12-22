6a97628231cf06977c9ae73b7e3eff91fdaa20d2
JDX50S <71799746+smoke-wolf@users.noreply.github.com>
2023-12-21 16:39:18 -0800

    Update FunctionRequest.py --->
    This is somewhere between 1.5 and 1.7

--- System/Drive/FunctionRequest.py
+++ System/Drive/FunctionRequest.py
@@ -1,8 +1,18 @@
+'''
+ is script creates a graphical user interface for an application
+ with buttons for various functionalities and repositories. It
+ also handles analytics and checks for updates or forced login
+ based on user preferences.
+ '''
+
+
+
 import inspect
 import uuid
 from tkinter import ttk
 
 import User.UserProfile
+import System.Drive.Errors_Events.EventMan as AR
 import System.Drive.Errors_Events.EventMan as EV
 import System.Drive.UI_Functions.Install
 
@@ -22,7 +32,15 @@ def Activate():
 
 
 def GUI():
+    import User.UserProfile as UP
+    if UP.Forced_Login:
+        import System.Drive.Login
+    else:
+        pass
+
     EV.guiEvent(4, '', inspect.currentframe().f_lineno, get_current_function(), False, True, 1)
+
+
     global directories
     directories = []
     import os
@@ -82,24 +100,39 @@ def GUI():
 
     global root
 
+    import tkinter as tk
+
     root = tk.Tk()
-    root.title('GHp Manager')
-    root.config(bg='#C5E0DC')
+    root.title('GHpm')
+
+    # Set the transparency level for the entire window (0 is fully transparent, 1 is fully opaque)
+    alpha_value = 0.93  # Adjust the alpha value as needed
+
+    root.attributes("-alpha", alpha_value)
+
+    # Create a canvas to act as the window's background with a colored rectangle
+    canvas = tk.Canvas(root, width=526, height=505)
+
+
+    # Set the canvas background color
+    bg_color = '#EE85B5'
+    canvas.create_rectangle(0, 0, 526, 505, fill=bg_color, outline="")
 
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
+    root.resizable(False, False)
 
     window_width = 526
-    window_height = 460
+    window_height = 505
     x_coordinate = (screen_width - window_width) // 2
     y_coordinate = (screen_height - window_height) // 2
     root.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
 
-    bg_color = '#C5E0DC'
-    button_color = '#A6D8CD'
-    button_hover_color = '#D8D8D8'
-    text_color = '#304B56'
-    text_color_other = '#81BDA4'
+
+    # Define colors
+    bg_color = '#7A3B69'
+    text_color = '#3c3293'
+    button_color = '#3c3293'
 
     button_font = ('Helvetica', 14)
 
@@ -159,8 +192,8 @@ def GUI():
 
     buttons = []
 
-    button_texts = ['Information', 'Settings', 'Install', 'Activate', 'Uninstall']
-    button_commands = [show_information, settings_window, show_install, Activate, show_list_window]
+    button_texts = ['Information', 'Settings', 'Install', 'Activate', 'Uninstall','CommandL']
+    button_commands = [show_information, settings_window, show_install, Activate, show_list_window,crypt]
 
     for text, command in zip(button_texts, button_commands):
         button = tk.Button(
@@ -175,35 +208,37 @@ def GUI():
         )
         button.pack(side='top', padx=15, pady=15)
         buttons.append(button)
-
+    import System.Drive.templates.main
     bottom_button = tk.Button(
         root,
-        text='CLI',
+        text='GhPm Recommended',
         bg='#C54034',
         fg='white',
         height=3,
-        width=12,
+        width=15,
         font=button_font,
-        command=crypt,
+        command=System.Drive.templates.main.main,
     )
     bottom_button.pack(side='bottom', padx=10, pady=15)
 
     # Peppermint theme colors
-    bg_color = "#C5E0DC"  # Light blue
-    text_color = "#2d047a"  # Dark gray
-    button_color = "#391e6b"  # Light teal
+    bg_color = '#EE85B5'
+    text_color = '#32936F'
+    button_color = '#EE85B5'
 
     # Border color for the frame
-    border_color = "#391e6b"  # A light gold color
+    border_color = "#EE85B5"  # A light gold color
 
     # Apply Peppermint theme to canvas and scrollbar
     style = ttk.Style()
+
     style.theme_use("clam")  # Using the "clam" theme for a modern look
-    style.configure("Vertical.TScrollbar", gripcount=0, background=bg_color, troughcolor=bg_color, bordercolor=bg_color)
+    style.configure("Vertical.TScrollbar", gripcount=0, background=bg_color, troughcolor=bg_color, bordercolor=bg_color, borderwidth=1)
     style.map("Vertical.TScrollbar", background=[("active", button_color), ("disabled", bg_color)])
 
     # Create a canvas and add a scrollbar
     canvas = tk.Canvas(root)
+
     scrollbar = ttk.Scrollbar(root, orient="vertical", command=canvas.yview, style="Vertical.TScrollbar")
     canvas.configure(yscrollcommand=scrollbar.set)
 
@@ -212,6 +247,11 @@ def GUI():
 
     # Create a frame inside the canvas to hold the content with a colored border
     content_frame = ttk.Frame(canvas, borderwidth=4, relief="solid")
+
+    style = ttk.Style()
+    style.configure("My.TFrame.TFrame", background="#EE85B5")  # Replace "blue" with your desired background color
+
+
     content_frame["style"] = "My.TFrame"  # Define a custom style for the frame
     canvas.create_window((0, 0), window=content_frame, anchor="nw")
 
@@ -241,7 +281,7 @@ def GUI():
             git_row_frame,
             text="Download",
             bg=button_color,
-            fg=text_color,
+            fg='#006b73',
             padx=10,
             pady=5,
             command=lambda: (System.Drive.UI_Functions.Install.Installer(value=repo_url)),
@@ -266,34 +306,36 @@ def GUI():
 
     git_rows = [
         {
-            "repo_name": "Thank You For Downloading!",
+            "repo_name": "Thank You For Downloading!                                                                                        ",
             "repo_url": "",
             "description": "Hope you enjoy using GHPM",
         },
         {
-            "repo_name": "alfred",
-            "repo_url": "https://github.com/Alfredredbird/alfred",
-            "description": "Alfred is a advanced OSINT information gathering tool",
+            "repo_name": "GHPM Powered Custom Shell                                                                                        ",
+            "repo_url": "https://github.com/smoke-wolf/GHPM-CUSTOM-SHELL",
+            "description": '''GHPM-CUSTOM-SHELL add-on for easy implementation
+            of shortcuts and user preference modifications within 
+            the terminal interface''',
         },
         {
-            "repo_name": "Requirement-Scanner",
+            "repo_name": "Requirement-Scanner                                                                                         ",
             "repo_url": "https://github.com/smoke-wolf/ReqScanner",
             "description": "Compile a list of all pip packages used in a python application",
         },
         {
-            "repo_name": "Local Weather",
+            "repo_name": "Local Weather                                                                                       ",
             "repo_url": "https://github.com/smoke-wolf/weather",
             "description": "Get a Local Weather Report",
         },
         {
-            "repo_name": "Touch Script",
+            "repo_name": "Touch Script                                                                                        ",
             "repo_url": "https://github.com/smoke-wolf/TouchScript",
             "description": '''.touch is a lightweight and user-friendly scripting language
         designed to automate common tasks
         on your computer.''',
         },
         {
-            "repo_name": "GPlock",
+            "repo_name": "GPlock                                                                                                              ",
             "repo_url": "https://github.com/smoke-wolf/GpLock",
             "description": "Compile a list of all pip packages used in a python application",
         },
@@ -305,27 +347,29 @@ def GUI():
         )
 
     # Define the custom style for the frame border
+    style = ttk.Style()
+    style.configure("My.TFrame", background="#EE85B5")  # Replace "blue" with your desired background color
     style.configure("My.TFrame", bordercolor=border_color)
 
     import User.UserProfile
+    try:
+        with open(f'{User.UserProfile.SourceDirectory}System/.Cache/User/analytics', 'r') as file:
+            data = file.read()
+            import requests
+            url = f'''https://gpm-web.vercel.app/analytics={data}'''
+            requests.get(url)
+    except:
+        pass
     EV.PushAnalytics(a1=uuid.uuid1().hex, a2='Login', a3='None')
     if User.UserProfile.AutoUpdate:
-
         try:
             import System.Drive.VersionUpdate
-            System.Drive.VersionUpdate
+            System.Drive.VersionUpdate()
 
         except:
-            if User.UserProfile.Forced_Login:
-                import System.Drive.Login
-                exec('System.Drive.Login')
 
             root.mainloop()
     else:
-        if User.UserProfile.Forced_Login:
-            import System.Drive.Login
-            exec('System.Drive.Login')
         root.mainloop()
-
 
 
