acc7ff7eb7f230bd8cc764c82cec7e175adc78f4
JDX50S <71799746+smoke-wolf@users.noreply.github.com>
2024-05-08 13:31:41 -0700

    Create Style.py

---
+++ System/Drive/PAMap/Style.py
@@ -0,0 +1,12 @@
+fg='#006b73'
+#'#7A3B69'
+bg_color = '#9c3293'
+bg_color_info = '#f0f0f0'
+text_color = '#3c3293'
+#button_color = '#3c3293'
+button_color = '#3c3293'
+button_font = ('Helvetica', 14)
+bg='#006b73'
+hover_color = "#2980b9"
+foreground="#2471ed"
+background='#006b73'
