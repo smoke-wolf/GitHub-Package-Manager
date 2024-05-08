f909226e61270a74cd03e630417316c1a6b92041
JDX50S <71799746+smoke-wolf@users.noreply.github.com>
2024-05-08 13:33:52 -0700

    Update Information.py

--- System/Drive/UI_Functions/Information.py
+++ System/Drive/UI_Functions/Information.py
@@ -1,29 +1,18 @@
-import datetime
 import inspect
-import platform
-import re
-import socket
-import subprocess
 from tkinter import ttk
 import User.UserProfile
-import System.Drive.Errors_Events.EventMan as EV
+import System.Drive.PAMap.Patch0 as p0
 import tkinter as tk
 from tkinter.font import Font
 cwd = User.UserProfile.SourceDirectory
-
-
+import System.Drive.PAMap.Style as A
 import System.Drive.Errors_Events.EventMan as EV
 import uuid
 EV.PushAnalytics(a1=uuid.uuid1().hex, a2='Information', a3='None')
-def get_current_function():
-    stack = inspect.stack()
-    frame = stack[1]
-    code = frame[0]
-    return code.f_code.co_name
 
 
 def show_information():
-    EV.guiEvent(4, '', inspect.currentframe().f_lineno, get_current_function(), False, True, 1)
+    EV.guiEvent(4, '', inspect.currentframe().f_lineno, p0.get_current_function(), False, True, 1)
 
     info_window = tk.Tk()
     info_window.title('Information')
@@ -42,28 +31,19 @@ def show_information():
     canvas = tk.Canvas(info_window, width=526, height=505)
 
     # Set the canvas background color
-    bg_color = '#EE85B5'
-    canvas.create_rectangle(0, 0, 526, 505, fill=bg_color, outline="")
+    bg_color = A.bg_color_info
+    canvas.create_rectangle(0, 0, 526, 505, fill='#f0f0f0', outline="")
 
     x_coordinate = (screen_width // 2) - (window_width // 2)
     y_coordinate = (screen_height // 2) - (window_height // 2)
     info_window.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
 
-    info_text = """
-        GHPM (GitHub Package Manager) is a lightweight package manager designed to efficiently manage coding projects and applications.
+    info_text = p0.Info()
 
-        Key Features:
-        - Simplified Logic: GHPM focuses on basic and straightforward logic, making it easy to use and understand.
-        - Code Management: It effectively manages coding projects, allowing you to organize and maintain your codebase.
-        - Package Installation: GHPM streamlines the installation of project requirements and dependencies.
-
-        GHPM is a powerful yet lightweight solution for managing your code projects. Give it a try and experience the simplicity and efficiency it offers!
-        """
-
-    container = tk.Frame(info_window, bg='#F2E8C4', bd=2, relief=tk.SOLID)
+    container = tk.Frame(info_window, bg=A.bg, bd=2, relief=tk.SOLID)
     container.pack(fill=tk.BOTH, expand=True, padx=20, pady=20)
 
-    text_widget = tk.Label(container, text=info_text, bg='#F2E8C4', fg='#7E4A35', justify=tk.LEFT, wraplength=400)
+    text_widget = tk.Label(container, text=info_text, bg='#f0f0f0', fg=A.text_color, justify=tk.LEFT, wraplength=400)
     text_widget.pack(fill=tk.BOTH, expand=True)
 
     # Apply text styling
@@ -74,5 +54,3 @@ def show_information():
     okay_button.pack(pady=10)
 
     info_window.mainloop()
-
-
