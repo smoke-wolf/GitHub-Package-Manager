1792034a51b28bf64738f5106e599cf0707e41b8
JDX50S <71799746+smoke-wolf@users.noreply.github.com>
2024-05-08 13:32:31 -0700

    Provides simple control for recolouring the ui.

--- System/Drive/PAMap/Style.py
+++ System/Drive/PAMap/Style.py
@@ -1,9 +1,7 @@
 fg='#006b73'
-#'#7A3B69'
 bg_color = '#9c3293'
 bg_color_info = '#f0f0f0'
 text_color = '#3c3293'
-#button_color = '#3c3293'
 button_color = '#3c3293'
 button_font = ('Helvetica', 14)
 bg='#006b73'
